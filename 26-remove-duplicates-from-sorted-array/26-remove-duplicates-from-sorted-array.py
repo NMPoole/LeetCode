4a5fdbc5608f4be1fafecb00ddbae8e9f5cb1219
Nathan Poole <105063981+NMPoole@users.noreply.github.com>
2022-07-06 19:01:35 +0100

    Time: 144 ms (25.78%), Space: 14.8 MB (53.22%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2:
+            return len(nums)
+        
+        i = 0  # Slow-run pointer.
+        for j, value in enumerate(nums):
+            if value == nums[i]:
+                continue
+            # Else, capture the result
+            i += 1
+            if i != j:
+                nums[i] = value # In place overriden.
+ 
+        return i + 1
+            
+        
