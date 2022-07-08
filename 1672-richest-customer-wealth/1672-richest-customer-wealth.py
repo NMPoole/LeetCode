2516289699c113c4d0525f4af642c00ec7469b99
Nathan Poole <105063981+NMPoole@users.noreply.github.com>
2022-07-08 11:47:45 +0100

    Time: 58 ms (46.19%), Space: 13.5 MB (51.63%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        # Pythonic Solution: 
+        #   Map applies sum() to each item in accounts. 
+        #   Each item is a list representing the wealth of a customer.
+        #   max() gets the max value of all the items returned by map().
+        return max(map(sum, accounts))
