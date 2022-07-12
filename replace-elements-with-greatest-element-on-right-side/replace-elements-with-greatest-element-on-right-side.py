a82f6028dffff7b0a746a332cef8afb23cbf513f
Nathan Poole <105063981+NMPoole@users.noreply.github.com>
2022-07-12 10:43:49 +0100

    Time: 101 ms, Memory: 14.8 MB - LeetHub

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -4,10 +4,13 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-        n = len(arr)
-        maxVal = arr[n-1]
-        arr[n-1] = -1
+        # Solution: O(n) time, O(1) space.
         
+        n = len(arr)
+        maxVal = arr[n-1] # Max value seen to the right of current index so far.
+        arr[n-1] = -1 # Last index always becomes -1 as nothing to the right.
+        
+        # Update all nums to max value seen to right so far.
         for i in range(n-2, -1, -1):
             temp = arr[i]
             arr[i] = maxVal
