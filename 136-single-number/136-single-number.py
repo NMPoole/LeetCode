52a0f5a4faabf933dae8648f57fb3c8685c2288f
Nathan Poole <105063981+NMPoole@users.noreply.github.com>
2022-07-14 14:24:10 +0100

    Time: 102 ms (96.63%), Space: 15.6 MB (80.22%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # Solution: O(n) time, O(1) additional space.
+        #   XOR of duplicate numbers zero-out, so result after XORing all nums is the one with no duplicate.
         result = nums[0]
         for i in range(1, len(nums)):
             result ^= nums[i]
