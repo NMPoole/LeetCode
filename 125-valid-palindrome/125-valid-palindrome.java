82884ea26106eb28e49ebe3634693992face7cba
Nathan Poole <105063981+NMPoole@users.noreply.github.com>
2022-07-14 14:19:00 +0100

    Time: 4 ms (89.31%), Space: 43.5 MB (67.06%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -4,6 +4,7 @@ class Solution {
         
         // Solution: O(n) time, O(1) additional space.
         
+        s = s.toLowerCase();
         int l = 0;
         int r = s.length() - 1;
         
@@ -14,7 +15,7 @@ class Solution {
             while (r > l && !Character.isLetterOrDigit(s.charAt(r))) r -= 1;
             
             // Check chars are the same on both sides.
-            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) return false;  
+            if (s.charAt(l) != s.charAt(r)) return false;  
             
             l += 1;
             r -= 1;
