8115e8a35d3fa9beb6add37b0032b17ecfbc76bb
Nathan Poole <105063981+NMPoole@users.noreply.github.com>
2022-07-14 14:23:28 +0100

    Time: 1 ms (100.00%), Space: 50.9 MB (35.91%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -2,6 +2,8 @@ class Solution {
     
     public int singleNumber(int[] nums) {
         
+        // Solution: O(n) time, O(1) additional space.
+        
         int result = nums[0];
         
         // XOR of duplicate numbers zero-out, so result after XORing all nums is the one with no duplicate.
