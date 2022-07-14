c0d1e08aedc878ad66d18a8a88f1b1d700add0f3
Nathan Poole <105063981+NMPoole@users.noreply.github.com>
2022-07-14 14:17:38 +0100

    Time: 5 ms (77.28%), Space: 44.3 MB (43.74%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -9,24 +9,12 @@ class Solution {
         
         while (l < r) {
             
-            char sl = s.charAt(l);
-            char sr = s.charAt(r);
-            
             // Skip over non-alphanumeric chars.
-            while (l < r && !Character.isLetterOrDigit(sl)) {
-                l += 1;
-                sl = s.charAt(l);
-            }
-                
-            while (r > l && !Character.isLetterOrDigit(sr)) {
-                r -= 1;
-                sr = s.charAt(r);
-            }
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) l += 1;
+            while (r > l && !Character.isLetterOrDigit(s.charAt(r))) r -= 1;
             
             // Check chars are the same on both sides.
-            if (Character.toLowerCase(sl) != Character.toLowerCase(sr)) {
-                return false;  
-            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) return false;  
             
             l += 1;
             r -= 1;
