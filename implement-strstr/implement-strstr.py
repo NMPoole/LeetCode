86a27f6b9b18fce896582b20ad8708296904ebb6
Nathan Poole <105063981+NMPoole@users.noreply.github.com>
2022-07-25 15:36:07 +0100

    Time: 19 ms, Memory: 13.6 MB - LeetHub

--- implement-strstr/implement-strstr.py
+++ implement-strstr/implement-strstr.py
@@ -6,4 +6,5 @@ class Solution(object):
         :rtype: int
         """
         # Solution: "O((n-m)*m) time - n is the size of haystack, and m is the size of needle."
+        # Problem is trivial in Python, but doesn't adhere to the point of the question.
         return haystack.find(needle)
