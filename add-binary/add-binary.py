70c2f3604c845007825c8d76d6d15b382a6cdc2d
Nathan Poole <105063981+NMPoole@users.noreply.github.com>
2022-07-25 15:27:19 +0100

    Time: 36 ms, Memory: 13.6 MB - LeetHub

---
+++ add-binary/add-binary.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        
+        # Solution: O(n) time, O(1) additional space.
+        
+        output = ""
+        carry = 0
+        
+        a, b = a[::-1], b[::-1]
+        
+        for i in range(max(len(a), len(b))):
+            
+            digitA = ord(a[i]) - ord("0") if i < len(a) else 0
+            digitB = ord(b[i]) - ord("0") if i < len(b) else 0
+            
+            total = digitA + digitB + carry
+            output = str(total % 2) + output
+            carry = total // 2
+            
+        if carry:
+            output = "1" + output
+            
+        return output
